1bba148827ffbf65344fc13ae42fa5e7ff0eeaaa
VolodymyrBab <volbv@yahoo.com>
2023-12-05 21:06:45 +0100

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -13,7 +13,7 @@ export const Filter =({onChange }) => {
             className={css.input}
             type="text"
             onChange={evt => onChange(evt.currentTarget.value)}
-          //   value={Filter.props.filter}
+        
           >
           </input>
         </label>
